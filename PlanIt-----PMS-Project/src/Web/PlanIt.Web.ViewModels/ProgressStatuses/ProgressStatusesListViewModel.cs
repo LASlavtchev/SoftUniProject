58b27aa43f2bf27ee0b6649a71406c7e7c1ea36f
Liubomir Slavtchev <lslavtchev@abv.bg>
2020-04-23 13:35:21 +0300

    admin-lte library upgrade

--- PlanIt-----PMS-Project/src/Web/PlanIt.Web.ViewModels/ProgressStatuses/ProgressStatusesListViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PlanIt.Web.ViewModels.ProgressStatuses
-{
-    using System.Collections.Generic;
-
-    public class ProgressStatusesListViewModel
-    {
-        public IEnumerable<ProgressStatusViewModel> ProgressStatuses { get; set; }
-    }
-}
