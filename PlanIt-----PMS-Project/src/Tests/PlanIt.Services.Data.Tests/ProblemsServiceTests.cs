850b1f832914cca2c437708f59dac448a64e09fd
Liubomir Slavtchev <lslavtchev@abv.bg>
2020-04-28 21:14:21 +0300

    Add Problems Initial tests

---
+++ PlanIt-----PMS-Project/src/Tests/PlanIt.Services.Data.Tests/ProblemsServiceTests.cs
@@ -0,0 +1,19 @@
+﻿namespace PlanIt.Services.Data.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.Extensions.DependencyInjection;
+    using PlanIt.Data.Models;
+    using PlanIt.Web.ViewModels.Administration.Invites;
+    using Xunit;
+
+    public class ProblemsServiceTests : BaseServiceTests
+    {
+        private IProblemsService ProblemsService => this.ServiceProvider.GetRequiredService<IProblemsService>();
+
+       
+    }
+}
